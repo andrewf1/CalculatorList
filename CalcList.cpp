a3e7a433b7b31f374e94dcafb8e8b1e7530f733b
Andrew Fernandez <fdez26@ymail.com>
2019-01-22 21:21:45 -0500

    Add removeLastOperation functionality

--- CalcList.cpp
+++ CalcList.cpp
@@ -17,6 +17,7 @@ CalcList::~CalcList() {
 void CalcList::newOperation(const FUNCTIONS func, const double operand) {
     CalcListNode* new_operation = new CalcListNode;
     new_operation->operation = func;
+    new_operation->operand = operand;
     new_operation->next = trailer;
     new_operation->prev = trailer->prev;
     trailer->prev = new_operation;
@@ -38,7 +39,27 @@ void CalcList::newOperation(const FUNCTIONS func, const double operand) {
 }
 
 void CalcList::removeLastOperation() {
-
+    CalcListNode* node_to_remove = trailer->prev;
+    //Do the opposite operation to restore previous total_val
+    switch(node_to_remove->operation) {
+        case ADDITION:
+            total_val -= node_to_remove->operand;
+            break;
+        case SUBTRACTION:
+            total_val += node_to_remove->operand;
+            break;
+        case MULTIPLICATION:
+            total_val /= node_to_remove->operand;
+            break;
+        case DIVISION:
+            total_val *= node_to_remove->operand;
+            break;
+    }
+    CalcListNode* prev_node = node_to_remove->prev;
+    CalcListNode* next_node = trailer;
+    prev_node->next = next_node;
+    trailer->prev = prev_node;
+    delete node_to_remove;
 }
 
 int main() {
--- CalcList.hpp
+++ CalcList.hpp
@@ -3,14 +3,19 @@
 
 #include "CalcListInterface.hpp"
 #include <string>
+#include <sstream>
+#include <iomanip>
 
-struct CalcListNode {
+class CalcListNode {
+public:
     CalcListNode() = default;
     ~CalcListNode() = default;
+private:
     CalcListNode* prev;
     CalcListNode* next;
     FUNCTIONS operation;
-    friend class CalcList;
+    double operand;
+friend class CalcList;
 };
 
 class CalcList : public CalcListInterface {
@@ -20,7 +25,7 @@ public:
     virtual double total() const override { return total_val; }
     virtual void newOperation(const FUNCTIONS func, const double operand) override;
     virtual void removeLastOperation() override;
-    virtual std::string toString(unsigned short precision) const override {}
+    virtual std::string toString(unsigned short precision) const override { return "hi"; }
     bool empty() { return header->next == trailer; }
 private:
     CalcListNode* header;
@@ -29,4 +34,4 @@ private:
 };
 
 
-#endif /* CALCLIST_HPP*/
+#endif /* CALCLIST_HPP */
