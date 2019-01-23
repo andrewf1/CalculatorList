7073f65355438408653700925cbb8f37f124cbba
Andrew Fernandez <fdez26@ymail.com>
2019-01-23 01:16:26 -0500

    Add comment documentation throughout source file

--- CalcList.cpp
+++ CalcList.cpp
@@ -1,7 +1,8 @@
 #include "CalcList.hpp"
 
+// Constructor
 CalcList::CalcList() {
-    total_val = 0.0;
+    total_val = 0.0; // Total should initially be zero
     header = new CalcListNode;
     trailer = new CalcListNode;
     header->next = trailer;
@@ -9,14 +10,16 @@ CalcList::CalcList() {
     size = 0;
 }
 
+// Destructor
 CalcList::~CalcList() {
     while(!empty()) {
-        removeLastOperation();
+        removeLastOperation(); // Removes all nodes except header and trailer
     }
     delete trailer;
     delete header;
 }
 
+// Add a new operation to the linked list
 void CalcList::newOperation(const FUNCTIONS func, const double operand) {
     CalcListNode* new_operation = new CalcListNode;
     new_operation->operation = func;
@@ -33,6 +36,7 @@ void CalcList::newOperation(const FUNCTIONS func, const double operand) {
         trailer->prev->next = new_operation;
         trailer->prev = new_operation;
     }
+    // Performing the arithmetic to change the total
     switch(func) {
         case ADDITION:
             total_val += operand;
@@ -50,16 +54,15 @@ void CalcList::newOperation(const FUNCTIONS func, const double operand) {
             break;
     }
     new_operation->node_total = total_val;
-    size++;
+    size++; // Keep track of number of elements in the linked list
 }
 
+// Remove the last operation from the linked list
 void CalcList::removeLastOperation() {
-    std::cout << "At beginning of function, size = " << size << std::endl;
     if(empty())
         throw("CannotRemoveFromEmptyList");
     CalcListNode* node_to_remove = trailer->prev;
-
-    //Do the opposite operation to restore previous total_val
+    // Do the opposite operation to restore previous total_val
     switch(node_to_remove->operation) {
         case ADDITION:
             total_val -= node_to_remove->operand;
@@ -68,7 +71,7 @@ void CalcList::removeLastOperation() {
             total_val += node_to_remove->operand;
             break;
         case MULTIPLICATION:
-            if(node_to_remove->operand == 0) {
+            if(node_to_remove->operand == 0) { // Removable Zero multiplication operation
                 total_val = node_to_remove->prev->node_total;
             }
             else {
@@ -86,23 +89,23 @@ void CalcList::removeLastOperation() {
         prev_node->next = next_node;
         trailer->prev = prev_node;
     }
-    else {
+    else { // Only one element to delete, set total_val back to 0.0 and point header and trailer to each other
         total_val = 0.0;
         header->next = trailer;
         trailer->prev = header;
     }
-    size--;
-    std::cout << "After the function, size = " << size << std::endl;
+    size--; // Decrement size to keep track of number of elements in the linked list
     delete node_to_remove;
 }
 
-//Have to implement set precision to the string still!!
+// Returns a string of list of operations at precision given as argument
 std::string CalcList::toString(unsigned short precision) const {
     std::ostringstream oSS;
     oSS << getOperationString(size, trailer->prev, precision);
     return oSS.str();
 }   
 
+// Recursive call to retrieve a string of the entire list
 std::string CalcList::getOperationString(unsigned int length, const CalcListNode* node, unsigned short prec) const{
     char op;
     switch(node->operation) {
@@ -121,8 +124,8 @@ std::string CalcList::getOperationString(unsigned int length, const CalcListNode
     }
     std::string op_strings = "";
     std::ostringstream oSS;
-    if(node->prev == header) {
-        double zero = 0.0000000000000000000000000000000000000000000000;
+    if(node->prev == header) { // Base case (1 node in the list)
+        double zero = 0.0000000;
         oSS << "1: " << std::setprecision(prec) << std::fixed << zero << op << node->operand << "=" << node->node_total;
         oSS << std::endl;
         op_strings += oSS.str();
@@ -137,27 +140,3 @@ std::string CalcList::getOperationString(unsigned int length, const CalcListNode
         return op_strings += getOperationString(length - 1, node->prev, prec);
     }
 }
-
-// int main() {
-    
-//     CalcList newList;
-//     newList.newOperation(ADDITION, 84.0);
-//     newList.newOperation(MULTIPLICATION, 87.0);
-//     newList.newOperation(SUBTRACTION, 78.0);
-//     newList.newOperation(DIVISION, 16);
-//     std::cout << newList.toString(3) << std::endl;
-//     newList.removeLastOperation(); //rm1
-//     std::cout << newList.toString(3) << std::endl;
-//     newList.removeLastOperation(); //rm2
-//     std::cout << newList.toString(3) << std::endl;
-//     newList.removeLastOperation(); //rm3 FAILS
-//     std::cout << newList.toString(3) << std::endl; //Getting 84 still when it should be 0
-//     newList.removeLastOperation(); //rm4
-//     if(newList.total() == 0) {
-//         std::cout << "You did something right" << std::endl;
-//     }
-//     else {
-//         std::cout << "Youre still messing up" << std::endl;
-//     }
-//     return 0;    
-// }
